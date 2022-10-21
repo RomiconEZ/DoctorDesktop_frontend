b89930d96488b4723acb41aaa73684bdf8426a02
Alina-Smol <78797567+Alina-Smol@users.noreply.github.com>
2022-10-21 21:37:09 +0300

    test

--- src/models/IUser.ts
+++ src/models/IUser.ts
@@ -1,5 +1,55 @@
-export interface IUser {
-    id: number;
-    name: string;
-    email: string;
+// export interface IUser {
+//     id: number;
+//     name: string;
+//     email: string;
+// }
+
+export interface Login {
+    email:string
+    password: string
 }
+export interface Login_response {
+    email:string
+    isActivated: boolean
+    id: string
+}
+
+export interface Registration {
+    email:string
+    password: string
+}
+export interface Registration_response  {
+    email:string
+    isActivated: boolean
+    id: string
+}
+
+export interface getAllPatients{
+    encryption: string
+    name: string
+    surname: string
+    patronymic: string
+    residenseRegion: string
+}
+export interface getAllPatients_response{
+    name: string
+    surname: string
+    patronymic: string
+    birthDate: string
+    sex: boolean
+    residenseRegion: string
+
+}
+
+export interface getDoctor{
+    name: string
+    surname: string
+    patronymic: string
+    birthDate: string
+    age: number
+    workExperience: number
+    residenseRegion: string
+    city: string
+    placeOfWork: string
+}
+
