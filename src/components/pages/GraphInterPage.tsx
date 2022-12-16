0b67f5b2356332692fbdd8371587c9bc53162ac5
Roman <roman.nieronov@mail.ru>
2022-12-16 16:03:56 +0300

    correct README.md

--- src/components/pages/GraphInterPage.tsx
+++ src/components/pages/GraphInterPage.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import DwvComponent from "../UI/DWV/DwvComponent";
 import './GraphInterStyle.css';
 import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { indigo, pink } from '@mui/material/colors';
+
 
 // const theme = createTheme({
 //     palette: {
