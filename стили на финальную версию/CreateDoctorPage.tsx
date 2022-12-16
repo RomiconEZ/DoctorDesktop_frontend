3c513e47ec6e7f2218b85f9602ed0cfd4ba3e2ca
Alina Smolina <78797567+Alina-Smol@users.noreply.github.com>
2022-12-16 17:24:48 +0300

    Create CreateDoctorPage.tsx

---
+++ стили на финальную версию/CreateDoctorPage.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {useAppSelector} from "../../hooks/redux";
+import CreateDoctorForm from "../UI/CreateDoctorForm";
+import useBreadcrumbs from "use-react-router-breadcrumbs";
+import {NavLink} from "react-router-dom";
+import {routes} from "../common/BreadCrumb";
+import Loader from "../common/Loader";
+
+
+
+
+const CreateDoctorPage = () => {
+    const {isLoading, error} = useAppSelector(state => state.userReducer);
+
+    const breadcrumbs = useBreadcrumbs(routes);
+
+    return (
+        <div className="ml-5 mb-10">
+            {isLoading && <Loader/>}
+            {error && <h1>Произошла ошибка при загрузке</h1>}
+            <>
+            {breadcrumbs.map(({ match, breadcrumb }) => (
+                <NavLink key={match.pathname} to={match.pathname} className="text-our-greenish-300 text-xs mr-1">
+                    /{breadcrumb}
+                </NavLink>
+            ))}
+            </>
+
+            <div>
+                <CreateDoctorForm />
+            </div>
+        </div>
+    );
+};
+
+export default CreateDoctorPage;
